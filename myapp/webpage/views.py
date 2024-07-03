1502a9a45f0a6ac4008fc2ddd39de87a2a4b966c
BuchPawares <125111002+BuchPawares@users.noreply.github.com>
2024-07-03 21:47:19 +0700

    com

---
+++ myapp/webpage/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render ,HttpResponse
+
+# Create your views here.
+def indexPage(request):
+    return render(request,'index.html')
+
+def aboutPage(request):
+    return HttpResponse("about Page")
